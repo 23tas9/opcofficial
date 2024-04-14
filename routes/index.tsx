110fb3626753348d39b6d4fe4fe5bac3afcc9a1c
たすくん <nizi.minato@gmail.com>
2024-04-14 22:20:17 +0900

    update: Contentsで管理

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,25 @@
 export default function Home() {
+  const Contents = [
+    {
+      "title": "目的",
+      "image": "https://placehold.jp/640x480.png",
+      "imageAlt": "ダミー",
+      "content": (  
+        <div>
+          <h2>目的</h2>
+          <p>
+            主に、プログラミングをゆるく楽しむサークルです！
+          </p>
+          <ul>
+            <li>プログラミングはわからないけど、興味がある</li>
+            <li>programmingの授業でわからないところがある</li>
+          </ul>
+          <p>などの初心者や</p>
+        </div>
+      )
+    }
+  ]
+
   return (
     <main>
       {/* hero */}
@@ -8,9 +29,18 @@ export default function Home() {
       </div>
 
       {/* Content wrapper */}
-      <div>
-        
-      </div>
+      <article>
+          {Contents.map(content=>(
+            <section>
+              <h2>{content.title}</h2>
+              {content.content}
+
+              <figure>
+                <img src={content.image} alt={content.imageAlt} />
+              </figure>
+            </section>
+          ))}
+      </article>
     </main>
   );
 }

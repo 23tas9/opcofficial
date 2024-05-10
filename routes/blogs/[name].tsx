0a17419eb76f2036a28cc7f2263c500c952c5897
たすくん <nizi.minato@gmail.com>
2024-05-10 19:57:45 +0900

    add: タイトルを表示するように

--- routes/blogs/[name].tsx
+++ routes/blogs/[name].tsx
@@ -2,13 +2,15 @@ import { Head } from "$fresh/runtime.ts";
 import { PageProps } from "$fresh/server.ts";
 import { CSS, render } from "https://deno.land/x/gfm@0.6.0/mod.ts";
 
+import { MDParser, Meta } from "../../tools/MDParser.ts";
+
 // syntax highlight from https://unpkg.com/browse/prismjs@1.29.0/components/
 
 import "https://esm.sh/prismjs@1.29.0/components/prism-c?no-check";
 import "https://esm.sh/prismjs@1.29.0/components/prism-cpp?no-check";
 import "https://esm.sh/prismjs@1.29.0/components/prism-python?no-check";
 
-const RenderPage = (body: string)=>{
+const RenderPage = (body: string, meta: Meta)=>{
     return (
         <>
             <Head>
@@ -17,7 +19,8 @@ const RenderPage = (body: string)=>{
                     {CSS}
                 </style>
             </Head>
-            <main>
+            <main class="center-align">
+                <h1>{meta.title}</h1>
                 <article data-color-mode="light" data-light-theme="light" data-dark-theme="dark" class="markdown-body"
                     dangerouslySetInnerHTML={{__html: body}}>
 
@@ -28,13 +31,16 @@ const RenderPage = (body: string)=>{
 }
 
 export default function Home(props: PageProps){
-    const path = `static/blogs/${props.params.name}.md`;
+    const path = `./static/blogs/${props.params.name}.md`;
 
     try{
         const text = Deno.readTextFileSync(path);
-        const body = render(text);
 
-        return RenderPage(body);
+        const md = MDParser(text);
+
+        const body = render(md.body);
+
+        return RenderPage(body, md.meta);
     }catch(e){
         return (
             <>

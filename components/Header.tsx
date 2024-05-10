fbb9f2b44e59ee1be11074a00a328bca3dcd4799
たすくん <nizi.minato@gmail.com>
2024-05-10 21:09:15 +0900

    add: レスポンシブ追加

--- components/Header.tsx
+++ components/Header.tsx
@@ -33,6 +33,14 @@ export function Header() {
           />
           <p>OECUPC</p>
         </a>
+        
+        {/* レスポンシブ時のボタン */}
+        <input type="checkbox" id="toggle-global-nav" />
+        <label for="toggle-global-nav" class="global-nav__toggle-label">
+          <span class="global-nav__toggle-label__animation-bar"></span>
+          <span class="global-nav__toggle-label__animation-bar"></span>
+        </label>
+
         {/* Global nav */}
         <nav className="global-nav">
           <ul className="global-nav__list">

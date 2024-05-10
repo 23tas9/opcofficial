c2faf8a4f8c76f409236f5e0c9c32b2611bdc7cb
たすくん <nizi.minato@gmail.com>
2024-05-10 23:42:23 +0900

    update: github-pagesの内容の追記など

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,13 @@
+import ExternalLink from "../components/ExternalLink.tsx";
+
+interface Activist {
+  year: number;
+  members: number;
+};
+
 export default function Home() {
+  const activists = JSON.parse(Deno.readTextFileSync("./static/data/activists.json"));
+  
   const Contents = [
     {
       "title": "目的",
@@ -12,8 +21,10 @@ export default function Home() {
           <ul>
             <li>プログラミングはわからないけど、興味がある</li>
             <li>programmingの授業でわからないところがある</li>
+            <li>自分で何か作品を作りたい！</li>
+            <li>競技プログラミングに挑戦してみたい！</li>
           </ul>
-          <p>などの初心者や</p>
+          <p>など、初心者から上級者のかたまで、大大大歓迎です！</p>
         </div>
       )
     },
@@ -24,12 +35,17 @@ export default function Home() {
       "content": (
         <div>
           <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
+            <li>プログラミングを楽しむ！</li>
+            <li>競技プログラミング(AtCoder等)</li>
+            <li>アプリ開発(Web/ネイティブ)</li>
+            <li>プログラミングの問題を解く</li>
+            <li>プログラミングの講座</li>
+            <li>情報系資格の勉強会</li>
           </ul>
+          <p>
+            などなど...<br />
+            このサイトも部員がDenoとFreshで制作しています！
+          </p>
         </div>
       )
     },
@@ -40,12 +56,15 @@ export default function Home() {
       "content": (
         <div>
           <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
+            <li>大阪電気通信大学 四条畷キャンパス(空き教室など)</li>
+            <li>大阪電気通信大学 寝屋川キャンパス(空き教室など)</li>
+            <li>オンライン(Discord等)</li>
           </ul>
+          <p>
+            サークルの部室はないので、<br />
+            空き教室やオンラインなどで活動しています！<br />
+            活動は自由参加ですので気軽に参加してください！
+          </p>
         </div>
       )
     },
@@ -56,12 +75,14 @@ export default function Home() {
       "content": (
         <div>
           <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
+            {activists["members_of_each_entered"]?.sort((a: Activist, b: Activist)=>{
+              return b.year - a.year;
+            })?.map((elem: Activist)=>{
+              if(elem.year === null) return;
+              return <li>{`${elem.year}年度入学生: ${elem.members}`}</li>
+            })}
           </ul>
+          <p>{`計${activists["total_members"]}名(${activists["latest_update"]}時点)`}</p>
         </div>
       )
     },
@@ -71,88 +92,14 @@ export default function Home() {
       "imageAlt": "ダミー",
       "content": (
         <div>
-          <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-          </ul>
+          <p>
+            <ExternalLink href="https://x.com/messages/compose?recipient_id=1527489590067490817" name="公式X(旧Twitter)のDM"/>、<br />
+            または代表や部員などのX(旧Twitter)のDMまでご連絡ください！
+          </p>
         </div>
       )
     }
   ];
-
-  const SubContents = [
-    {
-      "title": "サークルについて",
-      "list": [
-        {
-          "title": "1",
-          "href": "/"
-        },
-        {
-          "title": "2",
-          "href": "/"
-        },
-        {
-          "title": "3",
-          "href": "/"
-        },
-      ]
-    },
-    {
-      "title": "お知らせ・ブログ",
-      "list": [
-        {
-          "title": "1",
-          "href": "/"
-        },
-        {
-          "title": "2",
-          "href": "/"
-        },
-        {
-          "title": "3",
-          "href": "/"
-        },
-      ]
-    },
-    {
-      "title": "作品",
-      "list": [
-        {
-          "title": "1",
-          "href": "/"
-        },
-        {
-          "title": "2",
-          "href": "/"
-        },
-        {
-          "title": "3",
-          "href": "/"
-        },
-      ]
-    },
-    {
-      "title": "相談・入部等",
-      "list": [
-        {
-          "title": "1",
-          "href": "/"
-        },
-        {
-          "title": "2",
-          "href": "/"
-        },
-        {
-          "title": "3",
-          "href": "/"
-        },
-      ]
-    }
-  ]
 
   return (
     <main>
@@ -174,19 +121,6 @@ export default function Home() {
                <figure className="media-content__image">
                 <img src={content.image} alt={content.imageAlt} />
               </figure>
-            </section>
-          ))}
-        </section>
-
-        <section className="sub-content">
-          {SubContents.map(content=>(
-            <section>
-              <h2>{content.title}</h2>
-              <ul>
-                {content.list.map(element=>(
-                  <li><a href={element.href}>{element.title}</a></li>
-                ))}
-              </ul>
             </section>
           ))}
         </section>

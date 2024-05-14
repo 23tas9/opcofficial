d77c84b5c02115e764a81abfaa02755fc6d3d222
たすくん <nizi.minato@gmail.com>
2024-05-15 08:41:36 +0900

    fix: removeしたpost関連のものの反映

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,39 +4,37 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $_middleware from "./routes/_middleware.tsx";
+import * as $about from "./routes/about.tsx";
 import * as $activity from "./routes/activity.tsx";
 import * as $activity_name_ from "./routes/activity/[name].tsx";
 import * as $api_login from "./routes/api/login.ts";
-import * as $api_post from "./routes/api/post.ts";
+import * as $api_test from "./routes/api/test.tsx";
+import * as $api_testpost from "./routes/api/testpost.tsx";
 import * as $blog from "./routes/blog.tsx";
 import * as $blog_name_ from "./routes/blog/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $login from "./routes/login.tsx";
 import * as $logout from "./routes/logout.tsx";
-import * as $post from "./routes/post.tsx";
-import * as $Poster from "./islands/Poster.tsx";
+
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/_middleware.tsx": $_middleware,
+    "./routes/about.tsx": $about,
     "./routes/activity.tsx": $activity,
     "./routes/activity/[name].tsx": $activity_name_,
     "./routes/api/login.ts": $api_login,
-    "./routes/api/post.ts": $api_post,
+    "./routes/api/test.tsx": $api_test,
+    "./routes/api/testpost.tsx": $api_testpost,
     "./routes/blog.tsx": $blog,
     "./routes/blog/[name].tsx": $blog_name_,
     "./routes/index.tsx": $index,
     "./routes/login.tsx": $login,
     "./routes/logout.tsx": $logout,
-    "./routes/post.tsx": $post,
   },
-  islands: {
-    "./islands/Poster.tsx": $Poster,
-  },
+  islands: {},
   baseUrl: import.meta.url,
 } satisfies Manifest;
 

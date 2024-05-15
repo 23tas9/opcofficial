2d9824e1747aacf59132d6db0ac3daa5371f3c04
たすくん <nizi.minato@gmail.com>
2024-05-16 01:05:46 +0900

    update: 活動->イベント, イベントをPOSTリクエストでなく、DenoKVに

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,57 +1,89 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 
-import { ActivityInfo } from "../tools/utils.ts";
+interface ScheduledEventData {
+    id: string,
+    type: string,
+    location: string,
+    name: string,
+    start: Date,
+    end: Date | undefined,
+    status: number
+};
 
 interface Data{
-    currentActivities: ActivityInfo[] | undefined
+    scheduledEvent: ScheduledEventData[] | undefined
 };
 
 export const handler: Handlers<Data> = {
 	async GET(req, ctx) {
-        const kv = await Deno.openKv("test.db");
 
-        const data = await kv.get<[]>(["activities"]);
+        const kv = await Deno.openKv(`https://api.deno.com/databases/${Deno.env.get("DENO_KV_TARGET_UUID")}/connect`);
 
-        //console.log(data);
+        const data = await kv.get<ScheduledEventData[]>(["scheduledEvent"]);
+
+        // console.log(data);
 
         if(!data.value){
-            return ctx.render({ currentActivities: undefined });
+            return ctx.render({ scheduledEvent: undefined });
         }
         
-        return ctx.render({ currentActivities:
-            data.value
-        });
+        return ctx.render({ scheduledEvent: data.value });
 	},
 };
 
 export default function Home({ data }: PageProps<Data>){
-    //:console.log(data.currentActivities);
+
+    console.log(data.scheduledEvent.sort((a, b)=>{
+        return a.status - b.status;
+    }));
 
     return (
         <main>
-          {/* hero */}
-          <div className="hero">
-            <h1 className="hero__title">OECUPC</h1>
-            <p className="hero__description">OECU Programming Circle</p>
-          </div>
-          <div>
-            <h2>現在の活動</h2>
-            <ul>
-                {data.currentActivities?.map(activity=>(
-                    <li>
-                        <h3>{activity.type}</h3>
-                        <p>{activity.place}</p>
-                        <p>{new Date(activity.start).toLocaleString("ja-JP")}</p>
-                    </li>
-                ))}
-            </ul>
-          </div>
-          <div>
-            <h2>お知らせ</h2>
-            <ul>
-                <li></li>
-            </ul>
-          </div>
+            <div>
+                <h1 className="center-align">ホーム</h1>
+                <div>
+                    <h2>予定されているイベント</h2>
+                    <article className="scheduled-event">
+                    {data.scheduledEvent.length <= 0 ?
+                        <section className="scheduled-event__content center-align">
+                            <h3>現在予定されているイベントはありません。</h3>
+                        </section>
+                        :data.scheduledEvent.sort((a, b)=>{
+                            return b.status - a.status;
+                        }).map(event=>(
+                        <section className={
+                            "scheduled-event__content" + 
+                            (event.status === 2 ? " scheduled-event__content--active":"")
+                        }>
+                            <h3>{event.name}</h3>
+                            <p>場所: {event.location}</p>
+                            <p>開始予定時刻: {event.start.toLocaleString("ja-JP")}</p>
+                            <p>終了予定時刻: {event.end === undefined ?
+                                "未定":event.end.toLocaleString("ja-JP")
+                            }</p>
+                        </section>
+                    ))}
+                    </article>
+                </div>
+                <div>
+                    <h2>お知らせ</h2>
+                    <p>
+                        ここにお知らせが表示
+                    </p>
+                </div>
+                <div>
+                    <h2>サークルについて</h2>
+                    <p>主に、プログラミングをゆるく楽しむサークルです！</p>
+                    <ul>
+                        <li>プログラミングはわからないけど、興味がある</li>
+                        <li>programmingの授業でわからないところがある</li>
+                        <li>自分で何か作品を作りたい！</li>
+                        <li>競技プログラミングに挑戦してみたい！</li>
+                    </ul>
+                    <p>など、初心者から上級者のかたまで、大大大歓迎です！</p>
+                    <p>詳しくは<a href="/about">About</a>へ！</p>
+                </div>
+            </div>
         </main>
     );
 }
--- tools/utils.ts
+++ tools/utils.ts
@@ -2,20 +2,3 @@ export enum BlogType {
     blog = "blog",
     activity = "activity"
 };
-
-export enum ActivityInfoProcessType {
-    start = "start",
-    end = "end"
-};
-
-export enum ActivityInfoType {
-    offline = "offline",
-    online = "online"
-};
-
-export interface ActivityInfo{
-    type: ActivityInfoType,
-    place: string,
-    start: Date,
-    id: string,
-};

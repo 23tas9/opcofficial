01ad67f314dd34f471f4a218d82c214e223af29f
たすくん <nizi.minato@gmail.com>
2024-05-15 09:28:01 +0900

    add: postTester

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -9,8 +9,8 @@ import * as $activity from "./routes/activity.tsx";
 import * as $activity_name_ from "./routes/activity/[name].tsx";
 import * as $api_login from "./routes/api/login.ts";
 import * as $api_postCurrentActivity from "./routes/api/postCurrentActivity.tsx";
-//import * as $api_postEnd from "./routes/api/postEnd.tsx";
-//import * as $api_postNew from "./routes/api/postNew.tsx";
+import * as $api_postEnd from "./routes/api/postEnd.tsx";
+import * as $api_postNew from "./routes/api/postNew.tsx";
 import * as $blog from "./routes/blog.tsx";
 import * as $blog_name_ from "./routes/blog/[name].tsx";
 import * as $index from "./routes/index.tsx";
@@ -28,8 +28,8 @@ const manifest = {
     "./routes/activity/[name].tsx": $activity_name_,
     "./routes/api/login.ts": $api_login,
     "./routes/api/postCurrentActivity.tsx": $api_postCurrentActivity,
-    //"./routes/api/postEnd.tsx": $api_postEnd,
-    //"./routes/api/postNew.tsx": $api_postNew,
+    "./routes/api/postEnd.tsx": $api_postEnd,
+    "./routes/api/postNew.tsx": $api_postNew,
     "./routes/blog.tsx": $blog,
     "./routes/blog/[name].tsx": $blog_name_,
     "./routes/index.tsx": $index,

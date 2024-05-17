b0e7c6b9e8479447872c64a9e3e4549adbb2a381
たすくん <nizi.minato@gmail.com>
2024-05-18 00:34:05 +0900

    remove: ログイン機構の削除

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,13 +7,10 @@ import * as $_app from "./routes/_app.tsx";
 import * as $about from "./routes/about.tsx";
 import * as $activity from "./routes/activity.tsx";
 import * as $activity_name_ from "./routes/activity/[name].tsx";
-import * as $api_login from "./routes/api/login.ts";
 import * as $api_post from "./routes/api/post.ts";
 import * as $blog from "./routes/blog.tsx";
 import * as $blog_name_ from "./routes/blog/[name].tsx";
 import * as $index from "./routes/index.tsx";
-import * as $login from "./routes/login.tsx";
-import * as $logout from "./routes/logout.tsx";
 
 import { type Manifest } from "$fresh/server.ts";
 
@@ -24,13 +21,10 @@ const manifest = {
     "./routes/about.tsx": $about,
     "./routes/activity.tsx": $activity,
     "./routes/activity/[name].tsx": $activity_name_,
-    "./routes/api/login.ts": $api_login,
     "./routes/api/post.ts": $api_post,
     "./routes/blog.tsx": $blog,
     "./routes/blog/[name].tsx": $blog_name_,
     "./routes/index.tsx": $index,
-    "./routes/login.tsx": $login,
-    "./routes/logout.tsx": $logout,
   },
   islands: {},
   baseUrl: import.meta.url,

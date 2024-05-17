80fe7b9b9fcaa46d62add45cdc54a539948561e3
たすくん <nizi.minato@gmail.com>
2024-05-17 15:10:08 +0900

    dev: kv削除後元の状態に

--- routes/api/post.ts
+++ routes/api/post.ts
@@ -14,12 +14,6 @@ export const handler: Handlers = {
             const databaseUrl = Deno.env.get("KV_DATABASE_URL");
             const kv = await (databaseUrl ? Deno.openKv(databaseUrl):Deno.openKv());
 
-            for await(const entry of kv.list({ prefix: [] })){
-                console.log(`delete: ${entry.key}`);
-                kv.delete(entry.key);
-            }
-            return new Response(null, { status: 200 });
-
             const data = await kv.get<ScheduledEventData[]>(["scheduledEvent"]);
 
             const setValue = data.value?.some(event=> event.id === json.id) ?

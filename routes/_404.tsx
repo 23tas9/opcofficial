3698e5a64f956e55350939b4b5d6fe9790eedae8
たすくん <nizi.minato@gmail.com>
2024-11-27 11:24:34 +0900

    [fix]: mainについてるクラスを下の階層に

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -3,15 +3,17 @@ import { Head } from "$fresh/runtime.ts";
 export default function Error404() {
 	return (
 		<>
-			<Head>
+			<head>
 				<title>404 - Page not found</title>
-			</Head>
-			<main className="hero center-align">
-				<h1 className="hero__title">404 - Page not found</h1>
-				<p className="hero__description">
-					お探しのページは見つかりませんでした。
-				</p>
-				<a href="/">ホームに戻る</a>
+			</head>
+			<main>
+				<div  className="hero center-align">
+					<h1 className="hero__title">404 - Page not found</h1>
+					<p className="hero__description">
+						お探しのページは見つかりませんでした。
+					</p>
+					<a href="/">ホームに戻る</a>
+				</div>
 			</main>
 		</>
 	);
